aceaea72476db86f79bd9576195beb2f60ff8261
Gheorghe Cosmin Emilian <cosmin_emilian200@yhoo.com>
2022-06-29 19:08:26 +0300

    terminat functionalitatea

--- src/Components/Listare/Pet.js
+++ src/Components/Listare/Pet.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import Modal from "react-modal"
 import Axios from "axios";
 
 const Pet = (props) => {
+
+  const [modalIsOpen,setmodalIsOpen] = useState(false);
+
   const url = "https://petstore.swagger.io/v2/pet/" + props.index;
+
+
   function stergerePet() {
     console.log("happened");
     Axios.delete(url);
-    props.refresh();
+    setTimeout(props.refresh, 200);
   }
+
+  
   return (
     <tr>
       <td>{props.index}</td>
@@ -24,7 +32,13 @@ const Pet = (props) => {
         <Link to={"/editare/" + props.index} style={{ textDecoration: "none" }}>
           <button>Editaeaza</button>
         </Link>
-        <button onClick={stergerePet}>Sterge</button>
+        <button onClick={() => setmodalIsOpen(true)}>Sterge</button>
+        <Modal isOpen={modalIsOpen}>
+          <h2>Atentie!!!</h2>
+          <p>Petul va fi sters pentru totdeauna<br></br>Doresti sa continui?</p>
+          <button onClick={stergerePet}>Da</button>
+          <button onClick={() => setmodalIsOpen(false)}>Nu</button>
+        </Modal>
       </td>
     </tr>
   );
--- src/Components/Vizualizare/Pet.js
+++ src/Components/Vizualizare/Pet.js
@@ -2,16 +2,26 @@ import React from "react";
 
 const Pet = (props) => {
   console.log(props.data);
+  const data = props.data;
   return (
     <div>
-      <h2>{props.data.id + ". " + props.data.name}</h2>
-      <h2>{props.data.status}</h2>
-      {props.data.photoUrls.map((url) => (
+      <h2>{data.id + ". "  }{data.name !== undefined && data.name !== "" ? data.name : "Nu exista nume"}</h2>
+      {data.status !== "" && <h2>{data.status}</h2>}
+      {data.status === "" && <h2>"nu are status"</h2>}
+
+      {data.photoUrls[0] === "string"  && <p>Nu exista imagini</p>}
+      {data.photoUrls[0] !== "string" && data.photoUrls.map((url) => (
         <img src={url} style={{ maxWidth: "600px" }}></img>
       ))}
-      <h3>Category {props.data.category.id + ". "} {props.data.category.name}</h3>
-      {props.data.tags.map((tag) => (
-        <p>{tag.id+". "+tag.name}</p>
+
+      {data.category.id === 0 && data.category.name === "" && (
+        <h3>Nu exista categorii</h3>
+      )}
+      {(data.category.id !== 0 || data.category.name !== "") && (   <h3>    Category: {data.category.id + ". "} {data.category.name}    </h3>     )}
+      <h3>Tags:</h3>
+       {data.tags[0].id ===0 && (data.tags[0].name === "" || data.tags[0].name === 'string') && <p>Nu exista taguri</p>} 
+      {!(data.tags[0].id ===0 && (data.tags[0].name === "" || data.tags[0].name === 'string')) && data.tags.map((tag) => (
+        <p>{tag.id + ". " + tag.name}</p>
       ))}
     </div>
   );
